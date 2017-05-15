940a4109d6629cedef8151785e5d2df603137b65
Victor Santana Lemos <t203687@tce.es.gov.br>
2017-05-15 13:13:55 -0300

    criacao de classe base para os modelos. a mesma esta encarregada de toda logica de conexao e desconexao com o banco. vai ficar assim enquanto esse modelo atender

--- models/indicador.js
+++ models/indicador.js
@@ -1,31 +1,6 @@
-const sql = require('mssql/msnodesqlv8')
+const BaseModel = require('./baseModel')
 
-class Indicador {
-
-  constructor() {
-    this.config = {
-      database: 'SAPO',
-      server: 'tcees43',
-      driver: 'msnodesqlv8',
-      options: {
-        trustedConnection: true
-      }
-    };
-  }
-
-  execQuery(query) {
-    return new Promise((resolve, reject) => {
-      sql.connect(this.config).then(pool => {
-        return pool.request().query(query);
-      }).then(result => {
-        resolve(result.recordset);
-      }).catch(err => {
-        reject(err);
-      }).then(() => {
-        sql.close();
-      });
-    });
-  }
+class Indicador extends BaseModel {
 
   getAll() {
     return this.execQuery('SELECT * FROM Indicador');
