31b1e4bcfb81ad80c512b661065179f93c712d29
Victor Santana Lemos <t203687@tce.es.gov.br>
2017-05-25 15:52:38 -0300

    logica para aceitar as requests POST vindas do cliente

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/indicadores', require('./routes/indicadores'));
 app.use('/pilares', require('./routes/pilares'));
