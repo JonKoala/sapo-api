29ea83d860791b2ef5ee03ad9055d90c0755b711
JonKoala <victor.santana.lemos@gmail.com>
2017-06-02 15:59:21 -0300

    adicionando logica crud simples de itens

--- routes/itens.js
+++ routes/itens.js
@@ -41,4 +41,52 @@ router.get('/:id/full', (req, res) => {
     });
 });
 
+router.get('/subnivel/:id', (req, res) => {
+
+  let id = req.params.id;
+
+  model.item.findAll({where: {subnivel_id: id}})
+    .then(itens => {
+      res.send(itens);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
+router.post('/', (req, res) => {
+
+  let newItem = req.body;
+
+  model.item.create(newItem)
+    .then(item => {
+      res.send(item);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
+router.put('/', (req, res) => {
+
+  let item = req.body;
+
+  model.item.update(item, {where: {id: item.id}})
+    .then(() => {
+      res.send(item);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
+router.delete('/', (req, res) => {
+
+  let item = req.body;
+
+  model.item.destroy({where: {id: item.id}})
+    .then(() => {
+      res.send({deleted: true});
+    }).catch(err => {
+      res.send({deleted: false, error: err});
+    });
+});
+
 module.exports = router;
