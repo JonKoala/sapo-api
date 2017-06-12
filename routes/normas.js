7bca938bc0460c99d4094f2ed0165d003c8a7136
JonKoala <victor.santana.lemos@gmail.com>
2017-06-12 16:12:14 -0300

    adicionando logica crud simples de norma

--- routes/normas.js
+++ routes/normas.js
@@ -36,4 +36,40 @@ router.get('/:id/full', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+
+  let newNorma = req.body;
+
+  model.norma.create(newNorma)
+    .then(norma => {
+      res.send(norma);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
+router.put('/', (req, res) => {
+
+  let norma = req.body;
+
+  model.norma.update(norma, {where: {id: norma.id}})
+    .then(() => {
+      res.send(norma);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
+router.delete('/', (req, res) => {
+
+  let norma = req.body;
+
+  model.norma.destroy({where: {id: norma.id}})
+    .then(() => {
+      res.send({deleted: true});
+    }).catch(err => {
+      res.send({deleted: false, error: err});
+    });
+});
+
 module.exports = router;
