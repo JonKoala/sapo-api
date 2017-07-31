58a494a969aac7f0810dda46e8e9b437cb59d0e4
JonKoala <victor.santana.lemos@gmail.com>
2017-07-31 18:44:23 -0300

    movendo (de legacy) e adaptando logica para adicao de avaliacoes

--- routes/avaliacoes.js
+++ routes/avaliacoes.js
@@ -22,4 +22,45 @@ router.get('/indicador', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+
+  var data = req.body;
+  var avaliacao;
+
+  //create a new avaliacao
+  model.avaliacao.create({
+      indicador_id: data.avaliacao.indicador_id,
+      nome: data.avaliacao.nome,
+      objetivos: data.avaliacao.objetivos
+    }).then(newAvaliacao => {
+      //create objetosAvaliacao from the newly created avaliacao
+
+      avaliacao = newAvaliacao;
+      let objsAvaliacao = data.entidades.map(entidade => {
+        return {avaliacao_id: avaliacao.id, entidade_id: entidade};
+      });
+
+      return model.objetoAvaliacao.bulkCreate(objsAvaliacao);
+    }).then(() => {
+      //find all objetosAvaliacao related to that avaliacao
+
+      return model.objetoAvaliacao.findAll({attributes: ['id'], where: {avaliacao_id: avaliacao.id}});
+    }).then(objetosAvaliacao => {
+      //create notas for each newly created objetoAvaliacao
+
+      var notas = [];
+      var ids = objetosAvaliacao.map(objAvaliacao => { return objAvaliacao.id; });
+      ids.forEach(id => {
+        let newNotas = data.itens.map(item => { return {objeto_avaliacao_id: id, item_id: item, usuario_id: 0}; });
+        Array.prototype.push.apply(notas, newNotas);
+      });
+
+      return model.utils.bulkOperation(model.nota, model.nota.bulkCreate, notas);
+    }).then(() => {
+      res.send(avaliacao);
+    }).catch(err => {
+      res.send(err);
+    });
+});
+
 module.exports = router;
